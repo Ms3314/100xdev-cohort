aa18033414b3feea096ce4e5e533b4f120d98748
Samiuddin <mdsamiuddin2005@outlook.com>
2024-12-12 22:58:15 +0530

    need to think how do i send the data

--- week16-chatapplication/chatapp-be/dist/index.js
+++ week16-chatapplication/chatapp-be/dist/index.js
@@ -2,16 +2,35 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-let userCount = 0;
 let allSockets = [];
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount++;
-    console.log("User connected #" + userCount);
+    // console.log("User connected #" + userCount)
     socket.on("message", (message) => {
-        console.log("Message recieved " + message.toString());
-        allSockets.map((sock) => {
-            sock.send(message.toString() + "send from the server");
-        });
+        // when i get a message this is happening 
+        // the data is in this format like payload wageira type 
+        //{
+        //  type : "send"
+        //  payload : {
+        //    message : "",
+        //    room : "" ,
+        //    name : "",  
+        //  }
+        const mesg = JSON.parse(message.toString());
+        if (mesg.type === "send") {
+            const socketobj = {
+                socket: socket,
+                room: mesg.payload.room,
+                name: mesg.payload.name
+            };
+            allSockets.push(socketobj);
+            console.log("Message recieved " + message.toString());
+            allSockets.map((sock) => {
+                if (sock.room === mesg.payload.room) {
+                    console.log("this is what i will be giving to others", JSON.stringify(socketobj));
+                    //@ts-ignore
+                    sock.send(JSON.stringify(socketobj));
+                }
+            });
+        }
     });
 });
--- week16-chatapplication/chatapp-be/src/index.ts
+++ week16-chatapplication/chatapp-be/src/index.ts
@@ -2,18 +2,44 @@ import { WebSocketServer , WebSocket} from "ws";
 
 const wss = new WebSocketServer({port : 8080});
 
-let userCount = 0;
-let allSockets:WebSocket[] = [];
+
+type websocket = {
+    socket  : WebSocket ;
+    room : String 
+}
+let allSockets:websocket[] = [];
 
 wss.on("connection" , (socket) => {
-    allSockets.push(socket)
-    userCount++ ;
-    console.log("User connected #" + userCount)
+    
+    // console.log("User connected #" + userCount)
     socket.on("message" , (message) => {
-        console.log("Message recieved " + message.toString())
-        allSockets.map((sock) =>{
-            sock.send(message.toString() + "send from the server")
-        })  
+        // when i get a message this is happening 
+        // the data is in this format like payload wageira type 
+        //{
+        //  type : "send"
+        //  payload : {
+        //    message : "",
+        //    room : "" ,
+        //    name : "",  
+        //  }
+        const mesg = JSON.parse(message.toString())
+        if (mesg.type === "send") {
+         
+            const socketobj = {
+                socket : socket ,
+                room : mesg.payload.room,
+                name : mesg.payload.name
+            }
+            allSockets.push(socketobj)
+            console.log("Message recieved " + message.toString())
+            allSockets.map((sock) =>{
+                if(sock.room === mesg.payload.room) {
+                    console.log( "this is what i will be giving to others" , JSON.stringify(socketobj));
+                    //@ts-ignore
+                    sock.send(JSON.stringify(socketobj));
+                }
+            })  
+    }
     })
 })
 
